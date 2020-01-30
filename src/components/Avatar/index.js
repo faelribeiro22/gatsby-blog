b76d0626755de71da93fe8566b4ae99d64a91e58
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2020-01-29 21:25:32 -0300

    feat: new version gatsby

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import * as S from "./styled"
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "avatar-photo.jpg" }) {
+          childImageSharp {
+            fixed(width: 80, height: 80) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  )
+  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
+}
+
+export default Avatar
