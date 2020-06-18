1dda1d1987492ad292830354396f01dbf0fdef18
Rafael Ribeiro de Sousa <rafaelribeirodesousa@pop-os.localdomain>
2020-06-18 01:03:59 -0300

    feat: add links

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,9 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Linkedin } from "@styled-icons/fa-brands/Linkedin"
+
+const Icons = {
+  Github,
+  Linkedin,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,12 @@
+const links = [
+  {
+    label: "Github",
+    url: "http://github.com/faelribeiro22",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/rafael-ribeiro-de-sousa-8428b172/",
+  },
+]
+
+export default links
