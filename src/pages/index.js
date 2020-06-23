a0c1834efe92380aaed5014e397e0d73f6a231c7
Rafael Ribeiro de Sousa <rafaelribeirodesousa@pop-os.localdomain>
2020-06-22 21:17:33 -0300

    feat: add post md info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import PostItem from "../components/PostItem"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
@@ -8,6 +8,15 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma"
+    />
   </Layout>
 )
 
